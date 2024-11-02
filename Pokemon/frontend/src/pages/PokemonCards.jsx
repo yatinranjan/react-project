60f44ae8be73a609693bb989782869b8cb9db824
Jitendra Kumar Ram <154624917+yatinranjan@users.noreply.github.com>
2024-11-03 00:22:12 +0530

    create pokemon cards

--- Pokemon/frontend/src/pages/PokemonCards.jsx
+++ Pokemon/frontend/src/pages/PokemonCards.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const PokemonCards = ({pokemonData}) => {
+const PokemonCards = ({ pokemonData }) => {
   return (
-    <li className='pokemon-card'>
-        <figure>
-            <img src="" alt="" />
-        </figure>
-       </li>
-  )
-}
+    <li className="pokemon-card">
+      <figure>
+        <img
+          src={pokemonData.sprites.other.dream_world.front_default}
+          alt={pokemonData.name}
+          className="pokemon-image"
+        />
+      </figure>
+    </li>
+  );
+};
 
-export default PokemonCards
+export default PokemonCards;
